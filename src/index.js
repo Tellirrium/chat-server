be192b7ecb4ca190ed6c93538e87de513bdb865b
Tellirrium <artem.suvazu@gmail.com>
2019-06-03 12:29:56 +0300

    server.close

--- src/index.js
+++ src/index.js
@@ -3,13 +3,18 @@
 const Websocket = require('ws');
 
 const server = new Websocket.Server({ port: 3000 });
+const arrayOfClients = [];
 
 server.on('connection', (socket) => {
   console.log('client connected');
+  arrayOfClients.push(socket);
 
   socket.on('message', (data) => {
     if (data === 'end') {
       socket.close();
+      if (arrayOfClients.every(client => client.readyState !== Websocket.OPEN)) {
+        server.close();
+      }
     } else {
       server.clients.forEach((client) => {
         if (client.readyState === Websocket.OPEN) {
