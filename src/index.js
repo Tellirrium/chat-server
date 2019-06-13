a25751782094311a81b7c37ccd2859b3c091beee
Tellirrium <artem.suvazu@gmail.com>
2019-06-13 14:20:41 +0300

    add socket.io

--- src/index.js
+++ src/index.js
@@ -1,26 +1,82 @@
-/* eslint-disable no-restricted-syntax */
-// eslint-disable-next-line import/no-extraneous-dependencies
-const Websocket = require('ws');
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
 
-const server = new Websocket.Server({ port: 4000 });
-const arrayOfClients = [];
+const port = 4000;
 
-server.on('connection', (socket) => {
+const app = express();
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const server = require('http').createServer(app);
+const io = require('socket.io').listen(server);
+
+server.listen(port, () => console.log(`Server listen port ${port}`));
+
+const users = [];
+const connections = [];
+
+io.sockets.on('connection', (socket) => {
+  connections.push(socket);
   console.log('client connected');
-  arrayOfClients.push(socket);
+  // console.log(socket.id);
+
+  socket.on('disconnect', () => {
+    connections.splice(connections.indexOf(socket), 1);
+    console.log('client disconnected');
+  });
 
   socket.on('message', (data) => {
-    if (data === 'end') {
-      socket.close();
-      if (arrayOfClients.every(client => client.readyState !== Websocket.OPEN)) {
-        server.close();
-      }
-    } else {
-      server.clients.forEach((client) => {
-        if (client.readyState === Websocket.OPEN) {
-          client.send(data);
-        }
+    const time = (new Date()).toLocaleTimeString();
+
+    connections.forEach((client) => {
+      client.json.send({
+        event: 'message', text: data, time, id: socket.id,
       });
-    }
+    });
   });
 });
+
+app.post('/auth', (req, res) => {
+  const token = jwt.decode(req.body.data);
+  res.send(token);
+});
+
+// const server = require('socket.io').listen(4000);
+
+// const clients = [];
+
+// // server.set('log level', 1);
+
+// server.sockets.on('connection', (socket) => {
+//   console.log('client connected');
+//   clients.push(socket);
+
+//   const ID = (socket.id).toString().substr(0, 5);
+
+//   // const time = (new Date).toLocaleTimeString();
+
+//   // // Посылаем клиенту сообщение о том, что он успешно подключился и его имя
+//   // socket.json.send({'event': 'connected', 'name': ID, 'time': time});
+//   // // Посылаем всем остальным пользователям, что подключился новый клиент и его имя
+//   // socket.broadcast.json.send({'event': 'userJoined', 'name': ID, 'time': time});
+
+//   socket.on('message', (data) => {
+//     const time = (new Date()).toLocaleTimeString();
+
+//     clients.forEach((client) => {
+//       client.json.send({
+//         event: 'messageReceived', name: ID, text: data, time,
+//       });
+//     });
+//   });
+
+//   socket.on('disconnect', () => {
+//     const time = (new Date()).toLocaleTimeString();
+
+//     server.sockets.json.send({ event: 'userSplit', name: ID, time });
+//   });
+// });
